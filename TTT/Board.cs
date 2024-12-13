ca5a35c06c2dd49a8bb31423a0a331871521ce91
peterj-wl <peter.johansson@winlas.se>
2024-12-13 15:24:29 +0100

    test: GetRandomEmptySquare failing

--- TTT/Board.cs
+++ TTT/Board.cs
@@ -1,6 +1,14 @@
 ﻿namespace TTT;
 
-public class Board
+public sealed class Board
 {
+    private readonly char[] _squares = new char[9];
 
+    public int GetRandomEmptySquare()
+    {
+        var freeSquares = new List<int>();
+        
+        var random = new Random();
+        return freeSquares[random.Next(freeSquares.Count)];
+    }
 }
--- Test/Tests.cs
+++ Test/Tests.cs
@@ -1,10 +1,19 @@
-﻿namespace Test;
+﻿using TTT;
+
+namespace Test;
 
 public class Tests
 {
     [Fact]
-    public void Test1()
+    public void GetRandomEmptySquare_BoardHaveSlots_ShouldReturnEmptySquare()
     {
+        // Arrange
+        var board = new Board();
 
+        // Act
+        var availablePosition = board.GetRandomEmptySquare();
+
+        // Assert
+        Assert.InRange(availablePosition, 0, 8);
     }
 }
